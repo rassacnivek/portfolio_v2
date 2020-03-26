41d9a6463433c57750fb311a7894ba64ef0b5fd6
Richaudeau <contact.applications@groupetrefle.com>
2020-03-26 16:57:59 +0100

    add: loader and fade in

--- src/base/index.js
+++ src/base/index.js
@@ -2,16 +2,51 @@ import React, { Component } from 'react';
 import './base.scss';
 import Header from '../layout/Header';
 import Home from '../pages/Home';
-// import Footer from '../layout/Footer';
+import { Lottie } from '@crello/react-lottie';
+import animationData from '../assets/loader.json';
+import FadeIn from 'react-fade-in';
 
 class Base extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showLoader: true
+    }
+  }
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        showLoader: false
+      });
+    }, 1500);
+  }
+
   render() {
     return (
-      <div>
-        <Header/>
-        <Home/>
-        {/* <Footer/> */}
-      </div>
+      <>
+        {
+          this.state.showLoader ? (
+            <div className="loading"><div className="loader__container">
+              <Lottie
+                config={
+                  {
+                    animationData: animationData,
+                    loop: true
+                  }
+                }
+                className="loader"
+              />
+            </div>
+            </div>
+          ) : (
+              <FadeIn delay={250} transitionDuration={1000} className="container">
+                <Header />
+                <Home />
+              </FadeIn>
+            )
+        }
+      </>
     )
   }
 }
